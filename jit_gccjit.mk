cbd418b26acf3b5f3d3a93938f4c0b71ee5022a6
Daniel Schwen <daniel.schwen@inl.gov>
2020-02-06 10:48:14 -0700

    fix(?) gccjit makefile

--- jit_gccjit.mk
+++ jit_gccjit.mk
@@ -1,7 +1,8 @@
 # GCC JIT
-GCC_BIN ?= $(dir $(shell which gcc))
-GCCDIR ?= $(GCC_BIN)/..
+#GCC_BIN ?= $(dir $(shell which gcc))
+#GCCDIR ?= $(GCC_BIN)/..
 OBJS += SymbolicMathFunctionGCCJIT.o SymbolicMathNodeDataGCCJIT.o
 CONFIG := -DSYMBOLICMATH_USE_GCCJIT
-LDFLAGS := -lgccjit -L$(GCCDIR)/lib
-CPPFLAGS += -I$(GCCDIR)/include/
+LDFLAGS := -lgccjit
+#-L$(GCCDIR)/lib
+#CPPFLAGS += -I$(GCCDIR)/include/
