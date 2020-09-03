1ed5d0257b9355e24d9494f5152f5ea74392c0e4
Daniel Schwen <daniel.schwen@inl.gov>
2020-09-03 08:06:00 -0600

    update c compiled performance

--- SMCompiledLLVM.C
+++ SMCompiledLLVM.C
@@ -22,7 +22,7 @@
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/LegacyPassManager.h"
+//#include "llvm/IR/LegacyPassManager.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Type.h"
